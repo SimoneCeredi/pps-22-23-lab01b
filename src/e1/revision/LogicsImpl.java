0ffb5b347621a42e7ea43f8627ec9b10dceafad6
Simone Ceredi <ceredi.simone.iti@gmail.com>
2023-03-06 21:49:31 +0100

    modified logics to use the chessboard

--- src/e1/revision/LogicsImpl.java
+++ src/e1/revision/LogicsImpl.java
@@ -2,7 +2,6 @@ package e1.revision;
 
 import e1.Logics;
 import e1.Pair;
-import e1.revision.piece.Piece;
 import e1.revision.piece.PieceFactory;
 import e1.revision.piece.PieceFactoryImpl;
 
@@ -10,28 +9,27 @@ import java.util.Random;
 
 public class LogicsImpl implements Logics {
 
-    private final Piece pawn;
-    private final Piece knight;
+    private final Chessboard chessboard;
     private final int size;
     private final Random random = new Random();
     private final PieceFactory pieceFactory = new PieceFactoryImpl();
 
     public LogicsImpl(int fieldSize, Pair<Integer, Integer> pawn, Pair<Integer, Integer> knight) {
         this.size = fieldSize;
-        this.pawn = this.pieceFactory.createPawn(pawn);
-        this.knight = this.pieceFactory.createKnight(knight);
+        this.chessboard = new ChessboardImpl(size, this.pieceFactory.createPawn(pawn), this.pieceFactory.createKnight(knight));
     }
 
     public LogicsImpl(int size) {
         this.size = size;
-        this.pawn = this.pieceFactory.createPawn(randomEmptyPosition());
-        this.knight = this.pieceFactory.createKnight(randomEmptyPosition());
+        this.chessboard = new ChessboardImpl(size, this.pieceFactory.createPawn(randomEmptyPosition()),
+                this.pieceFactory.createKnight(randomEmptyPosition()));
     }
 
     private Pair<Integer, Integer> randomEmptyPosition() {
         Pair<Integer, Integer> pos = new Pair<>(this.random.nextInt(size), this.random.nextInt(size));
         // the recursive call below prevents clash with an existing pawn
-        return this.pawn != null && this.pawn.getLocation().equals(pos) ? randomEmptyPosition() : pos;
+        return this.chessboard.getPawn() != null && this.chessboard.getPawn().getLocation().equals(pos) ?
+                randomEmptyPosition() : pos;
     }
 
     @Override
@@ -39,16 +37,16 @@ public class LogicsImpl implements Logics {
         if (row < 0 || col < 0 || row >= this.size || col >= this.size) {
             throw new IndexOutOfBoundsException();
         }
-        return this.pawn.getLocation().equals(this.knight.move(row, col));
+        return this.chessboard.getPawn().getLocation().equals(this.chessboard.getKnight().move(row, col));
     }
 
     @Override
     public boolean hasKnight(int row, int col) {
-        return this.knight.getLocation().equals(new Pair<>(row, col));
+        return this.chessboard.getKnight().getLocation().equals(new Pair<>(row, col));
     }
 
     @Override
     public boolean hasPawn(int row, int col) {
-        return this.pawn.getLocation().equals(new Pair<>(row, col));
+        return this.chessboard.getPawn().getLocation().equals(new Pair<>(row, col));
     }
 }
