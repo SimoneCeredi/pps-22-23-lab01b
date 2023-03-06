6ee68fa51a70fa06d12e5367c8a81d97be5f293b
Simone Ceredi <ceredi.simone.iti@gmail.com>
2023-03-06 11:56:16 +0100

    Changed GUI to use revised logics

--- src/e1/revision/LogicsImpl.java
+++ src/e1/revision/LogicsImpl.java
@@ -6,17 +6,32 @@ import e1.revision.piece.Piece;
 import e1.revision.piece.PieceFactory;
 import e1.revision.piece.PieceFactoryImpl;
 
+import java.util.Random;
+
 public class LogicsImpl implements Logics {
 
     private final Piece pawn;
     private final Piece knight;
     private final int size;
+    private final Random random = new Random();
+    private final PieceFactory pieceFactory = new PieceFactoryImpl();
 
     public LogicsImpl(int fieldSize, Pair<Integer, Integer> pawn, Pair<Integer, Integer> knight) {
         this.size = fieldSize;
-        PieceFactory pieceFactory = new PieceFactoryImpl();
-        this.pawn = pieceFactory.createPawn(pawn);
-        this.knight = pieceFactory.createKnight(knight);
+        this.pawn = this.pieceFactory.createPawn(pawn);
+        this.knight = this.pieceFactory.createKnight(knight);
+    }
+
+    public LogicsImpl(int size) {
+        this.size = size;
+        this.pawn = this.pieceFactory.createPawn(randomEmptyPosition());
+        this.knight = this.pieceFactory.createKnight(randomEmptyPosition());
+    }
+
+    private final Pair<Integer, Integer> randomEmptyPosition() {
+        Pair<Integer, Integer> pos = new Pair<>(this.random.nextInt(size), this.random.nextInt(size));
+        // the recursive call below prevents clash with an existing pawn
+        return this.pawn != null && this.pawn.getLocation().equals(pos) ? randomEmptyPosition() : pos;
     }
 
     @Override
