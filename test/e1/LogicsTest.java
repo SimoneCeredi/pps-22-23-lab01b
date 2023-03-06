e0a85942731c1c83f4f74de14b884507aaa2d054
Simone Ceredi <ceredi.simone.iti@gmail.com>
2023-03-06 10:36:45 +0100

    Added test to check if knight exists

--- test/e1/LogicsTest.java
+++ test/e1/LogicsTest.java
@@ -4,19 +4,27 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LogicsTest {
     public static final int FIELD_SIZE = 5;
+    public static final Pair<Integer, Integer> PAWN = new Pair<>(0, 0);
+    public static final Pair<Integer, Integer> KNIGHT = new Pair<>(1, 1);
     private Logics logics;
 
     @BeforeEach
     void setUp() {
-        this.logics = new LogicsImpl(FIELD_SIZE);
+        this.logics = new LogicsImpl(FIELD_SIZE, PAWN, KNIGHT);
     }
 
     @Test
     void testCantMoveOutsideField() {
         assertThrows(IndexOutOfBoundsException.class, () -> this.logics.hit(FIELD_SIZE, 0));
+    }
+
+    @Test
+    void testKnightExists() {
+        assertTrue(() -> this.logics.hasKnight(KNIGHT.getX(), KNIGHT.getY()));
     }
 
 
