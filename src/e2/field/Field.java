91472838aa3948aa89eebd5eaf905f31b65f43ef
Simone Ceredi <ceredi.simone.iti@gmail.com>
2023-03-08 10:16:20 +0100

    Created base entities and relative tests

---
+++ src/e2/field/Field.java
@@ -0,0 +1,4 @@
+package e2.field;
+
+public interface Field {
+}
---
+++ test/e2/field/FieldTest.java
@@ -0,0 +1,12 @@
+package e2.field;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
