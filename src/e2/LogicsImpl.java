ec545fec4e03eaf2b2ba7caf75b5c6cfaba2ad58
Simone Ceredi <ceredi.simone.iti@gmail.com>
2023-03-08 16:07:22 +0100

    Refactoring

--- src/e2/LogicsImpl.java
+++ src/e2/LogicsImpl.java
@@ -12,4 +12,16 @@ public class LogicsImpl implements Logics {
     public int getSize() {
         return this.size;
     }
+
+//        private void addMines(int mines) {
+//        for (int i = 0; i < mines; i++) {
+//            Pair<Integer, Integer> position = this.randomEmptyPosition();
+//            this.field.put(position, this.cellFactory.mine());
+//        }
+//    }
+//
+//    private Pair<Integer, Integer> randomEmptyPosition() {
+//        Pair<Integer, Integer> position = new Pair<>(this.random.nextInt(size.getX()), this.random.nextInt(size.getY()));
+//        return this.field.get(position).isMine() ? this.randomEmptyPosition() : position;
+//    }
 }
--- src/e2/field/FieldImpl.java
+++ src/e2/field/FieldImpl.java
@@ -4,8 +4,6 @@ import e1.Pair;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class FieldImpl implements Field {
     private final Pair<Integer, Integer> size;
@@ -25,20 +23,6 @@ public class FieldImpl implements Field {
             }
         }
     }
-
-    // TODO: remove commented code
-//    private void addMines(int mines) {
-//        for (int i = 0; i < mines; i++) {
-//            Pair<Integer, Integer> position = this.randomEmptyPosition();
-//            this.field.put(position, this.cellFactory.mine());
-//        }
-//    }
-
-//    private Pair<Integer, Integer> randomEmptyPosition() {
-//        Pair<Integer, Integer> position = new Pair<>(this.random.nextInt(size.getX()), this.random.nextInt(size.getY()));
-//        return this.field.get(position).isMine() ? this.randomEmptyPosition() : position;
-//    }
-
 
     @Override
     public Pair<Integer, Integer> getSize() {
